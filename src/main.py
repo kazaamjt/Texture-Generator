4c0041115553fcd727d6c19b5d01a7e39d105091
Yaron Vanhulst <kazaamjt@gmail.com>
2020-10-02 00:29:41 +0200

    Add layer merging

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import math
 import sys
 import shutil
 from pathlib import Path
+from typing import List
 
 import click
-from psd_tools import PSDImage
 from PIL import Image
+from psd_tools import PSDImage
+from psd_tools.api.layers import PixelLayer
 
 @click.command()
 @click.option('--in', prompt='Input path', help='The path to directory containing psd files')
@@ -42,37 +44,49 @@ def process_psd(file_path: Path, out_path: Path) -> None:
 	png_path = out_path.joinpath("/".join(file_path.parts[1:])).with_suffix(".png")
 	psd = PSDImage.open(file_path)
 
-	len_psd = len(psd)
-	if len_psd > 1:
-		width = nearest_square(len_psd)
+	base_layer = None
+	layers: List[PixelLayer] = []
+	for i, layer in enumerate(psd):
+		if layer.visible:
+			if base_layer is None:
+				base_layer = layer.composite()
+			elif "[md]" in layer.name:
+				base_layer.paste(layer.composite(), box=None)
+			else:
+				layers.append(base_layer)
+				base_layer = layer.composite()
+	layers.append(base_layer)
+
+	len_layers = len(layers)
+	if len_layers == 1:
+		png = layers[0]
+	elif len_layers > 1:
+		width = nearest_square(len_layers)
 		height = width
-		if len_psd <= width * (width - 1):
+		if len_layers <= width * (width - 1):
 			height = width - 1
 
-		new_image = Image.new('RGBA', (width * psd.width, height * psd.height))
-		for i, layer in enumerate(psd):
-			row = 0
-			column = 0
+		png = Image.new('RGBA', (width * layers[0].width, height * layers[0].height))
+		row = 0
+		column = 0
+		for i, layer in enumerate(layers):
 			if i != 0:
 				column, row = divmod(i, width)
 			box = (
 				row * psd.width,
 				column * psd.height,
-				(row + 1) * psd.width,
-				(column + 1) * psd.height,
 			)
-			new_image.paste(layer.composite(), box=box)
-		new_image.save(png_path)
-	else:
-		png = standardize_alpha(psd)
-		png.save(png_path)
+			png.paste(layer.composite(), box=box)
+
+	png = standardize_alpha(psd)
+	png.save(png_path)
 
 def standardize_alpha(psd_image: PSDImage) -> Image:
 	image = psd_image.composite()
 	width, height = image.size
 	for x in range(width):
 		for y in range(height):
-			r, g, b, a = image.getpixel((x,y))
+			_, _, _, a = image.getpixel((x,y))
 			if a == 0:
 				image.putpixel((x,y), (0, 0, 0, 0))
 
